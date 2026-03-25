d7e03de9f2c5c97757d9cc2b3534b336709d3852
aneeshs05 <aneeshappid@gmail.com>
2026-03-25 21:55:39 +0530

    Teacher POST Method

--- internal/api/handlers/teachers.go
+++ internal/api/handlers/teachers.go
@@ -7,47 +7,47 @@ import (
 	"sync"
 
 	"github.com/aneeshsunganahalli/ClassSync/internal/models"
+	"github.com/aneeshsunganahalli/ClassSync/internal/repository/sqlconnect"
 )
 
 var (
 	teachers = make(map[int]models.Teacher)
-	mutex = &sync.Mutex{}
-	nextId = 1
+	mutex    = &sync.Mutex{}
+	nextId   = 1
 )
 
 func init() { // No need to manually called init() in Golang
 	teachers[nextId] = models.Teacher{
-		ID: nextId,
+		ID:        nextId,
 		FirstName: "John",
-		LastName: "Jones",
-		Class: "9A",
-		Subject: "Math",
+		LastName:  "Jones",
+		Class:     "9A",
+		Subject:   "Math",
 	}
 
 	nextId++
 	teachers[nextId] = models.Teacher{
-		ID: nextId,
+		ID:        nextId,
 		FirstName: "Jack",
-		LastName: "Jones",
-		Class: "9A",
-		Subject: "Math",
+		LastName:  "Jones",
+		Class:     "9A",
+		Subject:   "Math",
 	}
 }
-
 
 func TeachersHandler(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
-		case http.MethodGet:
-			getTeachersHandler(w, r)
-		case http.MethodPost:
-			fmt.Println("Placeholder")
-		case http.MethodDelete:
-			fmt.Println("Placeholder")
-		case http.MethodPut:
-			fmt.Println("Placeholder")
-		case http.MethodPatch:
-			fmt.Println("Placeholder")
+	case http.MethodGet:
+		getTeachersHandler(w, r)
+	case http.MethodPost:
+		addTeachersHandler(w, r)
+	case http.MethodDelete:
+		fmt.Println("Placeholder")
+	case http.MethodPut:
+		fmt.Println("Placeholder")
+	case http.MethodPatch:
+		fmt.Println("Placeholder")
 	}
 }
 
@@ -59,7 +59,7 @@ func getTeachersHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.URL.Query())
 
 	// DEBUG: Look at your terminal when you refresh the browser
-    fmt.Printf("Filtering for: Last='%s', First='%s'\n", lastName, firstName)
+	fmt.Printf("Filtering for: Last='%s', First='%s'\n", lastName, firstName)
 
 	teacherList := make([]models.Teacher, 0, len(teachers))
 	for _, teacher := range teachers {
@@ -70,14 +70,64 @@ func getTeachersHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	response := struct {
-		Status string `json:"status"`
-		Count int `json:"count"`
-		Data []models.Teacher `json:"data"`
+		Status string           `json:"status"`
+		Count  int              `json:"count"`
+		Data   []models.Teacher `json:"data"`
 	}{
 		Status: "Success",
-		Count: len(teacherList),
-		Data: teacherList,
+		Count:  len(teacherList),
+		Data:   teacherList,
 	}
 	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func addTeachersHandler(w http.ResponseWriter, r *http.Request) {
+	db, err := sqlconnect.ConnectDb()
+	if err != nil {
+		return
+	}
+
+	defer db.Close()
+
+	var newTeachers []models.Teacher
+	err = json.NewDecoder(r.Body).Decode(&newTeachers)
+	if err != nil {
+		return
+	}
+
+	stmt, err := db.Prepare("INSERT INTO teachers (first_name, last_name, email, class, subject) VALUES (?,?,?,?,?)")
+	if err != nil {
+		http.Error(w, "Error parsing the SQL Query", http.StatusInternalServerError)
+		return
+	}
+	defer stmt.Close()
+
+	addedTeachers := make([]models.Teacher, len(newTeachers))
+	for i, teacher := range newTeachers {
+		res, err := stmt.Exec(teacher.FirstName, teacher.LastName, teacher.Email, teacher.Class, teacher.Subject)
+		if err != nil {
+			http.Error(w, "Error inserting into database", http.StatusInternalServerError)
+			return
+		}
+		lastId, err := res.LastInsertId()
+		if err != nil {
+			http.Error(w, "Error getting last insert ID", http.StatusInternalServerError)
+		}
+		teacher.ID = int(lastId)
+		addedTeachers[i] = teacher
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	response := struct {
+		Status string           `json:"status"`
+		Count  int              `json:"count"`
+		Data   []models.Teacher `json:"data"`
+	}{
+		Status: "Success",
+		Count: len(addedTeachers),
+		Data: addedTeachers,
+	}
 	json.NewEncoder(w).Encode(response)
 }
--- internal/models/teacher.go
+++ internal/models/teacher.go
@@ -1,9 +1,10 @@
 package models
 
 type Teacher struct {
-	ID int 
-	FirstName string
-	LastName string
-	Class string
-	Subject string
+	ID        int    `json:"id,omitempty"`
+	FirstName string `json:"first_name,omitempty"`
+	LastName  string `json:"last_name,omitempty"`
+	Email     string `json:"email,omitempty"`
+	Class     string `json:"class,omitempty"`
+	Subject   string `json:"subject,omitempty"`
 }
